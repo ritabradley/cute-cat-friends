930d21e0da746ab7938498c50560f32e89f47bfa
Rita Bradley <rita@ritabradley.dev>
2023-01-20 14:22:46 -0800

    change focus outline color

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ const SearchBar = ({ searchChange }) => {
     return (
         <div className='p-4'>
             <input
-                className='p-3 border border-pink-400 bg-violet-100 rounded-md shadow-md'
+                className='p-3 border border-pink-400 bg-violet-100 rounded-md shadow-md outline-violet-400'
                 type='search'
                 placeholder='search robots'
                 onChange={searchChange}
