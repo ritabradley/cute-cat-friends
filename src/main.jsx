da1ca32cedd3ed6c68dfff193d43167389542ace
Rita Bradley <rita@ritabradley.dev>
2023-04-12 23:10:45 -0700

    ✨ combine reducers

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
-import { searchCats } from './reducers';
+import { searchCats, requestCats } from './reducers';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import './index.css';
 
 const logger = createLogger();
 
-const store = createStore(searchCats, composeWithDevTools(applyMiddleware(logger)));
+const rootReducer = combineReducers({ searchCats, requestCats });
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware, logger)));
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
