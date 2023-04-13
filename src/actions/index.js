f8045897be0319d9033e33d4ff207e2b3ec2a190
Rita Bradley <rita@ritabradley.dev>
2023-04-12 23:08:31 -0700

    ✨ create `requestCats` action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
-import { CHANGE_SEARCH_TERM } from '../constants';
+import { CHANGE_SEARCH_TERM, REQUEST_CATS_PENDING, REQUEST_CATS_SUCCESS, REQUEST_CATS_FAILED } from '../constants';
 
 export const setSearchTerm = (text) => ({
     type: CHANGE_SEARCH_TERM,
     payload: text,
 });
+
+export const requestCats = () => (dispatch) => {
+    dispatch({ type: REQUEST_CATS_PENDING });
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => dispatch({ type: REQUEST_CATS_SUCCESS, payload: data }))
+        .catch((error) => dispatch({ type: REQUEST_CATS_FAILED, payload: error }));
+};
