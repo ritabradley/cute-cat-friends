2ef8b1885b79c594858f2970b3c7f13902de8c3a
Rita Bradley <rita@ritabradley.dev>
2023-04-12 23:09:52 -0700

    ✨ create new state and `requestCats` reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,32 @@
-import { CHANGE_SEARCH_TERM } from '../constants';
+import { CHANGE_SEARCH_TERM, REQUEST_CATS_PENDING, REQUEST_CATS_SUCCESS, REQUEST_CATS_FAILED } from '../constants';
 
-const initialState = {
-    cats: [],
+const initialStateSearch = {
     searchTerm: '',
 };
 
-export const searchCats = (state = initialState, action = {}) => {
+export const searchCats = (state = initialStateSearch, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_TERM:
             return { ...state, searchTerm: action.payload };
+        default:
+            return state;
+    }
+};
+
+const initialStateCats = {
+    cats: [],
+    isPending: false,
+    error: '',
+};
+
+export const requestCats = (state = initialStateCats, action = {}) => {
+    switch (action.type) {
+        case REQUEST_CATS_PENDING:
+            return { ...state, isPending: true };
+        case REQUEST_CATS_SUCCESS:
+            return { ...state, cats: action.payload, isPending: false };
+        case REQUEST_CATS_FAILED:
+            return { ...state, error: action.payload, isPending: false };
         default:
             return state;
     }
